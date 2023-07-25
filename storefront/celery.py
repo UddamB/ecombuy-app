4f9afd32f3e757a43d8279abd305972e268ac89f
Uddam Bhathal <Uddamsingh14@gmail.com>
2023-07-25 19:07:02 -0400

    Delete storefront directory

--- storefront/celery.py
+++
@@ -1,9 +0,0 @@
-import os
-from celery import Celery
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'storefront.settings.dev')
-
-# Setting up celery to run background tasks
-celery = Celery('storefront')
-celery.config_from_object('django.conf:settings', namespace='CELERY')
-celery.autodiscover_tasks()
