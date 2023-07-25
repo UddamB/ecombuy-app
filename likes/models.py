c897214b5db6115f3349570567553812316d16ac
Uddam Bhathal <Uddamsingh14@gmail.com>
2023-07-25 18:50:46 -0400

    Add files via upload

---
+++ likes/models.py
@@ -0,0 +1,11 @@
+from django.conf import settings
+from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
+
+class LikedItem(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
