bf13e48674eb4e1629cdc1fa125c7f39f1332c57
Uddam Bhathal <Uddamsingh14@gmail.com>
2023-07-25 18:54:14 -0400

    Add files via upload

---
+++ tags/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
+
+class TaggedItemManager(models.Manager):
+    def get_tags_for(self, obj_type, obj_id):
+        content_type = ContentType.objects.get_for_model(obj_type)
+
+        return TaggedItem.objects \
+            .select_related('tag') \
+            .filter(
+                content_type=content_type,
+                object_id=obj_id
+            )
+
+
+class Tag(models.Model):
+    label = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.label
+
+
+class TaggedItem(models.Model):
+    objects = TaggedItemManager()
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
